6488b48bffbae88821ab90ceea2783d0be516c04
Bader al dine srour <baderadsrour@gmail.com>
2024-08-23 00:06:42 +0300

    asfasf"\ "

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,25 @@ fn exec(filename: &str) -> Result<(), Box<dyn Error>> {
   
 
     // print segments
-   
+    let mut segments = vec![];
+    for segment in elf.segments() {
+        let address = segment.address();
+        let size = segment.size();
+        let offset = segment.file_range().0;
+        let length = segment.file_range().1;
+        let flags = segment.flags();
+        println!("#\taddress\t\tsize\toffset\tlength\tflags");
+        println!(
+            "{}\t{:#x}\t{}\t{:#x}\t{}\t{}",
+            segments.len(),
+            address,
+            size,
+            offset,
+            length,
+            format_flags(flags)
+        );
+        segments.push(segment);
+    }
     // determine base address
 
     // determine entry point
@@ -84,108 +102,5 @@ fn exec(filename: &str) -> Result<(), Box<dyn Error>> {
 
 fn main() -> Result<(), Box<dyn Error>> {
     // load ELF provided within the first argument
-    let mut file = fs::File::open(filename)?;
-    let fd = file.as_raw_fd();
-    let data = fs::read(filename)?;
-    let elf = elf::FileHeader32::<object::Endianness>::parse(&*data)?;
-    let endian = elf.endian()?;
-
-    let entry_point = elf.e_entry.get(object::Endianness::Little) as usize;
-    let program_headers = elf.program_headers(endian, &data)?;
-
-    let mut load_segments: Vec<ProgramHeader32<Endianness>> = vec![];
-    for segment in program_headers {
-        if segment.p_type.get(object::Endianness::Little) == 1 {
-            load_segments.push(segment.clone());
-        }
-    }
-
-    let mut allocated_memory_ptrs: Vec<*mut c_void> = vec![];
-    let mut base_address: usize = usize::MAX;
-
-    eprintln!("Segments");
-    eprintln!("# address size offset length flags");
-    let mut i = 0;
-    for load_segment in load_segments {
-        let p_offset = load_segment.p_offset(object::Endianness::Little) as usize;
-        let p_vaddr = load_segment.p_vaddr(object::Endianness::Little) as usize;
-        let p_memsz = load_segment.p_memsz(object::Endianness::Little) as usize;
-        let p_flags = load_segment.p_flags(object::Endianness::Little) as usize;
-        let p_filesz = load_segment.p_filesz(object::Endianness::Little) as usize;
-
-        let actual_addr = p_vaddr - (p_vaddr % PAGE_SIZE);
-        let actual_offset = p_offset - (p_offset % PAGE_SIZE);
-
-        let segment_perms = SegmentPerms::from_number(p_flags);
-
-        eprintln!(
-            "{} 0x{:x} {} 0x{:x} {} {}",
-            i,
-            p_vaddr,
-            p_memsz,
-            p_offset,
-            p_filesz,
-            segment_perms.to_string()
-        );
-        i += 1;
-
-        let p = unsafe {
-            mmap(
-                actual_addr as *mut c_void,
-                p_memsz,
-                ProtFlags::PROT_WRITE | ProtFlags::PROT_READ | segment_perms.to_flags(),
-                MapFlags::MAP_PRIVATE | MapFlags::MAP_ANONYMOUS | MapFlags::MAP_FIXED,
-                -1,
-                0,
-            )
-        };
-
-        if let Err(e) = p {
-            println!("{}", e);
-            std::process::exit(-1);
-        }
-        let p = p.unwrap();
-
-        file.seek(SeekFrom::Start(actual_offset as u64))?;
-        let mut segment = vec![0; p_filesz];
-        file.read_exact(&mut segment)?;
-
-        unsafe {
-            std::ptr::copy_nonoverlapping(segment.as_ptr(), p as *mut u8, p_filesz);
-            mprotect(p, p_memsz, segment_perms.to_flags())?;
-        }
-
-        if p_vaddr < base_address {
-            base_address = p_vaddr;
-        }
-
-        allocated_memory_ptrs.push(p);
-    }
-
-    eprintln!("Entry point {:x}", entry_point);
-    eprintln!("Base address {:x}", base_address);
-
-    let handler = SigHandler::SigAction(sigsegv_handler);
-    let action = SigAction::new(handler, SaFlags::SA_SIGINFO, SigSet::empty());
-    unsafe { sigaction(Signal::SIGSEGV, &action) }?;
-
-    Ok(runner::exec_run(base_address, entry_point))
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    unsafe {
-        PAGE_SIZE = sysconf(SysconfVar::PAGE_SIZE)
-            .unwrap_or_else(|_| panic!("can't set global PAGE_SIZE"))
-            .unwrap_or_else(|| panic!("can't set global PAGE_SIZE --- 2"));
-    }
-
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: {} <executable>", args[0]);
-        std::process::exit(1);
-    }
-
-    let filename = &args[1];
-    exec(filename).unwrap_or_else(|_| panic!("can't execute file"));
     Ok(())
 }
